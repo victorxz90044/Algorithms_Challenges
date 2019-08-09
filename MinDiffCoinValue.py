2d71fefcea928450d54fda706ca5b37c11c29032
Victor Morales <u0512418@ad.utah.edu>
2019-08-09 09:25:19 -0600

    init mindiff

---
+++ MinDiffCoinValue.py
@@ -0,0 +1,12 @@
+def MinDiffCoin(arr):
+# sort
+    sorted = arr.sort()
+#get the difference in the adjucant values
+    #xdiff =  for n in range(1,len(arr))]
+    for(n in arr):
+        [arr[n]-arr[n-1]
+     #if current is smaller then save. save the new one
+       
+#return the min difference
+
+
---
+++ SubMaxfrommin.js
@@ -0,0 +1,8 @@
+function subractMaxfromMin(arr){
+
+    let max = arr.max();
+    let min = arr.min();
+    return max-min;
+
+
+};
